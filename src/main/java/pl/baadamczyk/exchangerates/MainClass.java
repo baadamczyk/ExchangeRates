3065ded48b193fed5b3b88e6071a7973dd24702f
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2016-12-27 21:59:02 +0100

    Added init code to the main class

--- src/main/java/pl/baadamczyk/exchangerates/MainClass.java
+++ src/main/java/pl/baadamczyk/exchangerates/MainClass.java
@@ -1,9 +1,18 @@
 package pl.baadamczyk.exchangerates;
 
-/**
- *
+import pl.baadamczyk.exchangerates.ui.LoadingDialog;
+
+/*
  * @author baadamczyk
  */
 public class MainClass {
     
+    public static void main(String[] args) {
+        displayLoadingScreen();
+    }
+
+    private static void displayLoadingScreen() {
+        LoadingDialog loadingDialog = new LoadingDialog();
+        loadingDialog.setVisible(true);
+    }
 }
