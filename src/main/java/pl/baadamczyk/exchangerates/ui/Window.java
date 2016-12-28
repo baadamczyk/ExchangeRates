d8474a85648aa92744152cce225c1c66bb44448f
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2016-12-28 10:48:14 +0100

    Added Window superclass

--- src/main/java/pl/baadamczyk/exchangerates/ui/Window.java
+++ src/main/java/pl/baadamczyk/exchangerates/ui/Window.java
@@ -1,5 +1,6 @@
 package pl.baadamczyk.exchangerates.ui;
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 /**
@@ -7,4 +8,27 @@ import javax.swing.JFrame;
  */
 public abstract class Window extends JFrame {
     
+    public Window(int width, int height, String title) {
+        setWindowProperties(width, height, title);
+    }
+    public void setWindowProperties(int width, int height, String title) {
+        setWindowTitle(title);
+        setWindowSize(new Dimension(width, height));
+        centerWindow();        
+    }
+    
+    private void setWindowTitle(String title) {
+        this.setTitle(title);
+    }
+
+    private void setWindowSize(Dimension WindowSize) {
+        setPreferredSize(WindowSize);
+        setMaximumSize(this.getPreferredSize());
+        setMinimumSize(this.getPreferredSize());
+        setResizable(false);        
+    }
+
+    private void centerWindow() {
+        setLocationRelativeTo(null);
+    }
 }
