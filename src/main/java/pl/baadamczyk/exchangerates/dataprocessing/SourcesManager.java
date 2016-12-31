1d72b60dc2807446c7158fa8b83997cbc5b4e3d1
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2016-12-31 06:26:41 +0100

    SourcesManager now interhits from XMLHandler

--- src/main/java/pl/baadamczyk/exchangerates/dataprocessing/SourcesManager.java
+++ src/main/java/pl/baadamczyk/exchangerates/dataprocessing/SourcesManager.java
@@ -1,8 +1,56 @@
 package pl.baadamczyk.exchangerates.dataprocessing;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  * @author baadamczyk
  */
-public class SourcesManager {
+public class SourcesManager extends XMLHandler {   
     
+    private ArrayList<DataSource> SourceList = new ArrayList<>();
+    
+    public SourcesManager() {
+        parseSourceList();
+    }
+
+    public ArrayList<DataSource> getSourceList() {
+        return SourceList;
+    }    
+
+    private void parseSourceList() {                     
+        try {
+            NodeList SourceNodesList = createNodeList(new File("src/main/resources/xml/Sources.xml"), "Source");
+            aquireContent(SourceNodesList);                                          
+        } catch (IOException | ParserConfigurationException | SAXException ex) {            
+        }
+    }
+
+    
+
+    private void aquireContent(NodeList SourceNodesList) {
+        
+    for(int i=0; i< SourceNodesList.getLength(); i++) {
+                Node ElementNode = SourceNodesList.item(i);
+                
+                if(ElementNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element SourceElement = (Element) ElementNode;                    
+                    createSourcesList(SourceElement);                                                            
+                }                                
+            }
+    }
+
+    private void createSourcesList(Element SourceElement) {
+        String SourceType = SourceElement.getElementsByTagName("Type").item(0).getTextContent();
+        String SourceBaseUnit = SourceElement.getElementsByTagName("BaseUnit").item(0).getTextContent();
+        String SourceName = SourceElement.getElementsByTagName("Name").item(0).getTextContent();
+        String SourceAddress = SourceElement.getElementsByTagName("Address").item(0).getTextContent();
+        SourceList.add(new DataSource(SourceType, SourceBaseUnit, SourceName, SourceAddress));
+    }
 }
--- src/main/java/pl/baadamczyk/exchangerates/dataprocessing/XMLHandler.java
+++ src/main/java/pl/baadamczyk/exchangerates/dataprocessing/XMLHandler.java
@@ -1,9 +1,24 @@
 package pl.baadamczyk.exchangerates.dataprocessing;
 
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author baadamczyk
  */
-public abstract class XMLHandler {
-    
+public class XMLHandler {
+    public NodeList createNodeList(File parsedFile, String rootTagName) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = builderFactory.newDocumentBuilder();            
+        Document sourcesXML = documentBuilder.parse(parsedFile);
+        
+        return sourcesXML.getElementsByTagName(rootTagName);
+    }
 }
